5ded329fb34b6f84237baee6ee7945a98ef6016f
Karl George <karlgeorge87@gmail.com>
2025-04-14 15:51:41 +0200

    Add Clerk auth

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,44 @@
+import { ClerkLoaded, ClerkProvider, useAuth } from '@clerk/clerk-expo';
+import { tokenCache } from '@clerk/clerk-expo/token-cache';
 import { Stack, usePathname, useRouter, useSegments } from 'expo-router';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Text, View } from 'react-native';
+import '@/global.css';
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+if (!publishableKey) {
+  throw new Error(
+    'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env'
+  );
+}
 
 const InitialLayout = () => {
   const router = useRouter();
   const pathname = usePathname();
   const segments = useSegments();
+
+  const { isLoaded, isSignedIn } = useAuth();
+
+  useEffect(() => {
+    if (!isLoaded) return;
+
+    const inAuthGroup = segments[0] === '(auth)';
+
+    if (isSignedIn && !inAuthGroup) {
+      router.replace('/(auth)/(tabs)');
+    } else if (!isSignedIn && pathname !== '/') {
+      router.replace('/');
+    }
+  }, [isSignedIn, isLoaded]);
+
+  if (!isLoaded) {
+    return (
+      <View className='items-center justify-center flex-1'>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
 
   return (
     <>
@@ -17,7 +51,13 @@ const InitialLayout = () => {
 };
 
 const RootLayout = () => {
-  return <InitialLayout />;
+  return (
+    <ClerkProvider tokenCache={tokenCache}>
+      <ClerkLoaded>
+        <InitialLayout />
+      </ClerkLoaded>
+    </ClerkProvider>
+  );
 };
 
 export default RootLayout;
