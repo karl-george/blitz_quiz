7759abfa4157f0bdf593d22e64025a9d11597c2f
Karl George <karlgeorge87@gmail.com>
2025-04-14 13:44:39 +0200

    Init commit

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import '@/global.css';
+import { Text, View } from 'react-native';
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Text className='text-4xl text-red-500'>Hello World!</Text>
+    </View>
+  );
+}
