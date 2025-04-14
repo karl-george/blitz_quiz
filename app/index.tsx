7e4fcaab2459acac8f312e11aac79c48071cac79
Karl George <karlgeorge87@gmail.com>
2025-04-14 14:03:36 +0200

    Setup file structure

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,10 @@
 import '@/global.css';
 import { Text, View } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
 
 export default function Index() {
   return (
-    <View className='items-center justify-center flex-1'>
-      <LinearGradient
-        colors={['#CCB6FF', '#986BFF']}
-        className='absolute top-0 bottom-0 left-0 right-0'
-      />
-      <Text className='text-4xl text-red-500'>Hello World!</Text>
+    <View>
+      <Text>Sign In</Text>
     </View>
   );
 }
