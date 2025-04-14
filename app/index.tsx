a1e247d02d4bd6a56b6cbb363ae07bb93654945b
Karl George <karlgeorge87@gmail.com>
2025-04-14 13:58:11 +0200

    Test gradient as background

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,14 @@
 import '@/global.css';
 import { Text, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
+    <View className='items-center justify-center flex-1'>
+      <LinearGradient
+        colors={['#CCB6FF', '#986BFF']}
+        className='absolute top-0 bottom-0 left-0 right-0'
+      />
       <Text className='text-4xl text-red-500'>Hello World!</Text>
     </View>
   );
