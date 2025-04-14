d0743dac6a7c7f29009632172080ec4ea6ba38fc
Karl George <karlgeorge87@gmail.com>
2025-04-14 13:57:59 +0200

    Fix gradient colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,16 @@ module.exports = {
   content: ['./app/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        grad_start: '#CCB6FF',
+        grad_end: '#986BFF',
+        button: '#C0B5F8',
+        correct: '#1EC751',
+        wrong: '#E93030',
+        highlight: '#7740F2',
+      },
+    },
   },
   plugins: [],
 };
