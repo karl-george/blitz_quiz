928d922d9bc164f48862396c292d0c7bec490fee
Karl George <karlgeorge87@gmail.com>
2025-04-15 12:37:18 +0200

    Add button bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,8 @@ module.exports = {
       colors: {
         grad_start: '#CCB6FF',
         grad_end: '#986BFF',
-        button: '#C0B5F8',
+        light_bg: '#C0B5F8',
+        border_light: '#D7D2F2',
         correct: '#1EC751',
         wrong: '#E93030',
         highlight: '#7740F2',
