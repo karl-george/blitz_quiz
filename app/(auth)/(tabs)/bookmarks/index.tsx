88fc089f136e1aca6ffa2f870aa97ba0753655b0
Karl George <karlgeorge87@gmail.com>
2025-04-16 15:59:26 +0200

    Add bookmarks flatlist

--- app/(auth)/(tabs)/bookmarks/index.tsx
+++ app/(auth)/(tabs)/bookmarks/index.tsx
@@ -1,10 +1,44 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import Button from '@/components/Button';
+import Question from '@/components/Question';
+import { tempBookmarks } from '@/constants/data';
+import { LinearGradient } from 'expo-linear-gradient';
+import React, { useState } from 'react';
+import { FlatList, Text, View } from 'react-native';
 
 const Page = () => {
+  const [extended, setExtended] = useState(false);
+
+  const bookmarks = tempBookmarks;
+
+  const handlePress = (e) => {
+    console.log(e);
+    setExtended((prevState) => !prevState);
+  };
+
   return (
-    <View>
-      <Text>Page</Text>
+    <View className='flex-1 px-4'>
+      <LinearGradient
+        colors={['#CCB6FF', '#986BFF']}
+        className='absolute top-0 bottom-0 left-0 right-0'
+      />
+      {/* Show bookmarked questions */}
+      <View className='pt-8'>
+        <FlatList
+          data={bookmarks}
+          contentContainerClassName='gap-4'
+          renderItem={({ item }) => (
+            <Question
+              question={item.question}
+              answer={item.answer}
+              variant='fullWidth'
+              onPress={(e) => handlePress(e)}
+              extended={extended}
+            />
+          )}
+          keyExtractor={(item) => item.id.toString()}
+          showsVerticalScrollIndicator={false}
+        />
+      </View>
     </View>
   );
 };
