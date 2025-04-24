cdf5e405bbd5748ffa12260fb8a1aad81560a312
Karl George <karlgeorge87@gmail.com>
2025-04-24 15:19:55 +0200

    Add question interface

---
+++ types.d.ts
@@ -0,0 +1,12 @@
+export interface Question {
+  question_id: number;
+  category_id: number;
+  question_text: string;
+  option_1: string;
+  option_2: string;
+  option_3: string;
+  option_4: string;
+  correct_option: string;
+  created_at: string;
+  updated_at: string;
+}
