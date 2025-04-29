8efef6f2d47e70d0eab19a18838112d083d08159
Karl George <karlgeorge87@gmail.com>
2025-04-29 11:42:27 +0200

    Use data from db

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,10 @@
+import { User } from '@/types';
 import { useUser } from '@clerk/clerk-expo';
 import { LinearGradient } from 'expo-linear-gradient';
 import React from 'react';
 import { Image, Text, View } from 'react-native';
 
-const Header = () => {
+const Header = ({ userData }: { userData: User }) => {
   const { user } = useUser();
 
   return (
@@ -11,7 +12,7 @@ const Header = () => {
       <View className='flex-row items-center'>
         <Image src={user?.imageUrl} className='rounded-full h-14 w-14' />
         <Text className='flex-1 ml-4 text-2xl font-semibold text-white'>
-          {user?.fullName}
+          {userData?.username}
         </Text>
         <View className='flex-row items-center gap-2 px-4 py-2 overflow-hidden border-2 border-white rounded-full'>
           <LinearGradient
@@ -19,7 +20,9 @@ const Header = () => {
             className='absolute top-0 bottom-0 left-0 right-0 '
           />
           <Text className='text-xl'>💎</Text>
-          <Text className='text-xl font-semibold text-white'>1235</Text>
+          <Text className='text-xl font-semibold text-white'>
+            {userData?.total_score}
+          </Text>
         </View>
       </View>
     </View>
