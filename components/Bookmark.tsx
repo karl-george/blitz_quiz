9610e6c6d5eb99eb86744370c00008a3288761c3
Karl George <karlgeorge87@gmail.com>
2025-04-29 14:15:36 +0200

    Add delete button

--- components/Bookmark.tsx
+++ components/Bookmark.tsx
@@ -1,19 +1,14 @@
+import { Ionicons } from '@expo/vector-icons';
 import React, { useState } from 'react';
-import { Pressable, Text } from 'react-native';
+import { Pressable, Text, TouchableOpacity, View } from 'react-native';
 
 interface BookmarkProps {
   question: string;
   answer: string;
-  variant: 'fullWidth';
+  handleDelete: () => void;
 }
 
-const styles = {
-  fullWidth: {
-    width: 'w-full',
-  },
-};
-
-const Bookmark = ({ question, answer, variant }: BookmarkProps) => {
+const Bookmark = ({ question, answer, handleDelete }: BookmarkProps) => {
   const [extended, setExtended] = useState(false);
 
   const handlePress = () => {
@@ -23,9 +18,22 @@ const Bookmark = ({ question, answer, variant }: BookmarkProps) => {
   return (
     <Pressable
       onPress={handlePress}
-      className={`items-center ${styles[variant].width} text-center border-2 border-border_light bg-light_bg rounded-xl justify-center py-6`}
+      className={`items-center w-full text-center border-2 border-border_light bg-light_bg rounded-xl justify-center py-6`}
     >
-      <Text className={`text-xl font-semibold  text-white`}>{question}</Text>
+      <Text className={`text-xl font-semibold text-white`}>{question}</Text>
+      {extended && (
+        <TouchableOpacity
+          className='absolute bottom-2 right-2'
+          onPress={handleDelete}
+        >
+          <Ionicons
+            name='trash'
+            size={24}
+            color={'#fff'}
+            onPress={handleDelete}
+          />
+        </TouchableOpacity>
+      )}
       {extended && (
         <Text className={`text-xl font-semibold  text-white pt-6`}>
           {answer}
