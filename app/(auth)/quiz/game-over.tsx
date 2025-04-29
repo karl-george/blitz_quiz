f94d4072a4ec61a2f0efdfc72f1dc3350f21b5d8
Karl George <karlgeorge87@gmail.com>
2025-04-29 13:50:32 +0200

    Add temp home screen link

--- app/(auth)/quiz/game-over.tsx
+++ app/(auth)/quiz/game-over.tsx
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native';
 import React from 'react';
+import { Link } from 'expo-router';
 
 const Page = () => {
   return (
     <View className='items-center justify-center flex-1'>
       <Text>GAME OVER</Text>
+      <Link href='/(auth)/(tabs)/home'>Home</Link>
     </View>
   );
 };
