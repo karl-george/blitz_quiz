945d766f685af042019f886634970a58639c7361
Karl George <karlgeorge87@gmail.com>
2025-05-07 10:05:41 +0200

    Add loading indicator
    -Clean up useeffect
    -Add loading state
    -Display indicator when loading

--- app/(auth)/(tabs)/leaderboard/index.tsx
+++ app/(auth)/(tabs)/leaderboard/index.tsx
@@ -6,16 +6,27 @@ import { useUser } from '@clerk/clerk-expo';
 import { LinearGradient } from 'expo-linear-gradient';
 import { Stack } from 'expo-router';
 import React, { useEffect, useState } from 'react';
-import { FlatList, Image, Text, View } from 'react-native';
+import { ActivityIndicator, FlatList, Image, Text, View } from 'react-native';
 
 const Page = () => {
   const { user } = useUser();
   const [userData, setUserData] = useState<User>();
   const [players, setPlayers] = useState<User[]>([]);
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
+        // Early return if user not found
+        if (!user?.id) {
+          setError('User not found');
+          return;
+        }
+
+        setLoading(true);
+
+        // Retrieve user
         const { data: userData, error: userError } = await supabase
           .from('users')
           .select(`*`)
@@ -23,12 +34,15 @@ const Page = () => {
           .single();
 
         if (userError) {
-          console.error(userError);
+          setError(
+            userError.message || 'An error occurred while fetching user data'
+          );
           return;
         }
 
         setUserData(userData ?? {});
 
+        // Retrieve players
         const { data: playerData, error: playerError } = await supabase
           .from('users')
           .select(`*`)
@@ -43,6 +57,8 @@ const Page = () => {
         setPlayers(playerData ?? []);
       } catch (error) {
         console.error(error);
+      } finally {
+        setLoading(false);
       }
     };
 
@@ -50,6 +66,8 @@ const Page = () => {
   }, []);
 
   useEffect(() => {
+    // Check if user is in the top 20
+    // If not, remove last player and add user
     if (!players.find((player) => player?.username === userData?.username)) {
       setPlayers((prevPlayers) => {
         const newPlayers = [...prevPlayers];
@@ -66,102 +84,110 @@ const Page = () => {
         colors={['#CCB6FF', '#986BFF']}
         className='absolute top-0 bottom-0 left-0 right-0'
       />
-      <Stack.Screen
-        options={{
-          header: () => <Header userData={userData!} />,
-        }}
-      />
-      {/* Top 3 */}
-      <View className='pt-6 mb-12'>
-        <View className='flex-row justify-center gap-4'>
-          <View className='mt-8'>
-            <Image
-              source={require('@/assets/images/icon.png')}
-              className='rounded-xl w-28 h-28'
-              width={100}
-              height={100}
-              resizeMode='cover'
-            />
-            <View className='p-2 mt-2 border-2 border-border_light bg-light_bg rounded-xl'>
-              <Text
-                className='font-bold text-center text-white'
-                numberOfLines={1}
-              >
-                {players[0]?.username}
-              </Text>
-              <Text
-                className='text-base text-center text-white'
-                numberOfLines={1}
-              >
-                💎 {players[0]?.total_score}
-              </Text>
-            </View>
-          </View>
-          <View>
-            <Image
-              source={require('@/assets/images/icon.png')}
-              className='w-32 h-32 rounded-xl'
-              width={100}
-              height={100}
-              resizeMode='cover'
-            />
-            <View className='p-2 mt-2 border-2 border-border_light bg-light_bg rounded-xl'>
-              <Text
-                className='font-bold text-center text-white'
-                numberOfLines={1}
-              >
-                {players[1]?.username}
-              </Text>
-              <Text
-                className='text-base text-center text-white'
-                numberOfLines={1}
-              >
-                💎 {players[1]?.total_score}
-              </Text>
-            </View>
-          </View>
-          <View className='mt-8'>
-            <Image
-              source={require('@/assets/images/icon.png')}
-              className='rounded-xl w-28 h-28'
-              width={100}
-              height={100}
-              resizeMode='cover'
-            />
-            <View className='p-2 mt-2 border-2 border-border_light bg-light_bg rounded-xl'>
-              <Text
-                className='font-bold text-center text-white'
-                numberOfLines={1}
-              >
-                {players[2]?.username}
-              </Text>
-              <Text
-                className='text-base text-center text-white'
-                numberOfLines={1}
-              >
-                💎 {players[2]?.total_score}
-              </Text>
-            </View>
-          </View>
+      {loading ? (
+        <View className='items-center justify-center flex-1'>
+          <ActivityIndicator size='large' color='#fff' />
         </View>
-      </View>
-
-      {/* Leaderboard list after top 3 */}
-      <FlatList
-        data={players.slice(3)}
-        contentContainerClassName='gap-2 pb-4'
-        showsVerticalScrollIndicator={false}
-        keyExtractor={(item, index) => index.toString()}
-        renderItem={({ item, index }) => (
-          <LeaderboardCard
-            key={index}
-            rank={index + 4}
-            name={item?.username}
-            score={item?.total_score}
-            user={userData!}
+      ) : (
+        <View className='flex-1'>
+          <Stack.Screen
+            options={{
+              header: () => <Header userData={userData!} />,
+            }}
           />
-        )}
-      />
+          {/* Top 3 */}
+          <View className='pt-6 mb-12'>
+            <View className='flex-row justify-center gap-4'>
+              <View className='mt-8'>
+                <Image
+                  source={require('@/assets/images/icon.png')}
+                  className='rounded-xl w-28 h-28'
+                  width={100}
+                  height={100}
+                  resizeMode='cover'
+                />
+                <View className='p-2 mt-2 border-2 border-border_light bg-light_bg rounded-xl'>
+                  <Text
+                    className='font-bold text-center text-white'
+                    numberOfLines={1}
+                  >
+                    {players[0]?.username}
+                  </Text>
+                  <Text
+                    className='text-base text-center text-white'
+                    numberOfLines={1}
+                  >
+                    💎 {players[0]?.total_score}
+                  </Text>
+                </View>
+              </View>
+              <View>
+                <Image
+                  source={require('@/assets/images/icon.png')}
+                  className='w-32 h-32 rounded-xl'
+                  width={100}
+                  height={100}
+                  resizeMode='cover'
+                />
+                <View className='p-2 mt-2 border-2 border-border_light bg-light_bg rounded-xl'>
+                  <Text
+                    className='font-bold text-center text-white'
+                    numberOfLines={1}
+                  >
+                    {players[1]?.username}
+                  </Text>
+                  <Text
+                    className='text-base text-center text-white'
+                    numberOfLines={1}
+                  >
+                    💎 {players[1]?.total_score}
+                  </Text>
+                </View>
+              </View>
+              <View className='mt-8'>
+                <Image
+                  source={require('@/assets/images/icon.png')}
+                  className='rounded-xl w-28 h-28'
+                  width={100}
+                  height={100}
+                  resizeMode='cover'
+                />
+                <View className='p-2 mt-2 border-2 border-border_light bg-light_bg rounded-xl'>
+                  <Text
+                    className='font-bold text-center text-white'
+                    numberOfLines={1}
+                  >
+                    {players[2]?.username}
+                  </Text>
+                  <Text
+                    className='text-base text-center text-white'
+                    numberOfLines={1}
+                  >
+                    💎 {players[2]?.total_score}
+                  </Text>
+                </View>
+              </View>
+            </View>
+          </View>
+
+          {/* Leaderboard list after top 3 */}
+          <FlatList
+            data={players.slice(3)}
+            contentContainerClassName='gap-2 pb-4'
+            showsVerticalScrollIndicator={false}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({ item, index }) => (
+              <LeaderboardCard
+                key={index}
+                rank={index + 4}
+                name={item?.username}
+                score={item?.total_score}
+                user={userData!}
+              />
+            )}
+          />
+        </View>
+      )}
     </View>
   );
 };
